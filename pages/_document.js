8f0d88cf3bb0adbecaf502d2a3b105585a555aae
Margaux <margaux@ordinatemargaux.mynetworksettings.com>
2025-07-04 12:58:43 -0400

    fix userback integration

--- pages/_document.js
+++ pages/_document.js
@@ -4,18 +4,14 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            window.Userback = window.Userback || {};
-            Userback.access_token = "A-Qfy1lzGmoDrw3qDf0KLZg2gYE";
-            (function(d) {
-              var s = d.createElement('script');
-              s.async = true;
-              s.src = 'https://static.userback.io/widget/v1.js';
-              (d.head || d.body).appendChild(s);
-            })(document);
-          `
-        }} />
+        <script src="https://static.userback.io/widget/v1.js"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              Userback.init('A-Qfy1lzGmoDrw3qDf0KLZg2gYE');
+            `
+          }}
+        />
       </Head>
       <body>
         <Main />
